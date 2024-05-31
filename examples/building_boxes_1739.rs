85e7d226f80f3ae31511130950ae5d69dd1d2499
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-05-30 22:59:09 -0400

    fixed implantation and by accounting for len in all indexes. added parallel support with rayon

--- examples/building_boxes_1739.rs
+++ examples/building_boxes_1739.rs
@@ -44,8 +44,8 @@ pub fn minimum_boxes(n: usize) -> usize {
     let mut floor_box_count = 0;
 
     for _box_number in 1..=n {
-        let valid_indexes = valid_indexes(&storage_room);
-        let (i, j, k) = find_best_index(valid_indexes).expect("there is always a valid index");
+        let valid_indexes = valid_indexes(&storage_room).collect::<Vec<_>>();
+        let (i, j, k) = find_best_index(&valid_indexes).expect("there is always a valid index");
         storage_room[i][j][k] = true;
 
         // println!("Box #{} placed at ({}, {}, {})\n", _box_number, i, j, k);
@@ -74,9 +74,9 @@ pub fn minimum_boxes(n: usize) -> usize {
 /// - and the lowest average between the other subindexes
 /// - Returns [None] if `indexes` is empty
 fn find_best_index(
-    indexes: impl Iterator<Item = (usize, usize, usize)>,
+    indexes: &[(usize, usize, usize)],
 ) -> Option<(usize, usize, usize)> {
-    indexes.fold(None, |best, current| match best {
+    indexes.iter().cloned().fold(None, |best, current| match best {
         None => Some(current),
         Some(best) => {
             let current_average = (current.1 + current.2) as f64 / 2.0;
@@ -97,24 +97,107 @@ fn find_best_index(
 }
 
 /// Returns an iterator yielding all indexes.
-fn all_indexes() -> impl Iterator<Item = (usize, usize, usize)> {
-    (0..3).flat_map(move |i| (0..3).flat_map(move |j| (0..3).map(move |k| (i, j, k))))
+fn all_indexes(len: usize) -> impl Iterator<Item = (usize, usize, usize)> {
+    (0..len).flat_map(move |i| (0..len).flat_map(move |j| (0..len).map(move |k| (i, j, k))))
 }
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 /// Returns an iterator yielding each valid spot's index
 fn valid_indexes<'a>(
     storage_room: &'a Vec<Vec<Vec<bool>>>,
 ) -> impl Iterator<Item = (usize, usize, usize)> + 'a {
-    const MAX_INDEX: usize = 2;
+    let max_index: usize = storage_room.len() - 1;
 
     // filter out any indexes that can't support a box
-    return all_indexes().filter(move |&(i, j, k)| {
+    return all_indexes(max_index).filter(move |&(i, j, k)| {
         // where are the walls?
         let is_wall_below = i == 0;
         let is_wall_left = j == 0;
-        let is_wall_right = j == MAX_INDEX;
+        let is_wall_right = j == max_index;
         let is_wall_behind = k == 0;
-        let is_wall_infront = k == MAX_INDEX;
+        let is_wall_infront = k == max_index;
+
+        // is current index occupied?
+        if storage_room[i][j][k] {
+            return false;
+        }
+
+        // is the current index supported by the floor?
+        if is_wall_below {
+            // println!("({}, {}, {}) is supported by the floor", i, j, k);
+            return true;
+        }
+
+        // is the current index supported by another box below the current index?
+        if !storage_room[i - 1][j][k] {
+            return false;
+        }
+
+        // is the box below supported on all four sides?
+        if (is_wall_left || storage_room[i - 1][j - 1][k])
+            && (is_wall_right || storage_room[i - 1][j + 1][k])
+            && (is_wall_behind || storage_room[i - 1][j][k - 1])
+            && (is_wall_infront || storage_room[i - 1][j][k + 1])
+        {
+            // println!("({}, {}, {}) is supported by the box below", i, j, k);
+            return true;
+        }
+
+        return false;
+    });
+}
+
+pub fn minimum_boxes_par(n: usize) -> usize {
+    // true means there is a box at that spot
+    // outer most collection is height
+    let mut storage_room = vec![vec![vec![false; n]; n]; n];
+
+    let mut floor_box_count = 0;
+
+    for _box_number in 1..=n {
+        let valid_indexes = valid_indexes(&storage_room).collect::<Vec<_>>();
+        let (i, j, k) = find_best_index(&valid_indexes).expect("there is always a valid index");
+        storage_room[i][j][k] = true;
+
+        // println!("Box #{} placed at ({}, {}, {})\n", _box_number, i, j, k);
+
+        if i == 0 {
+            floor_box_count += 1;
+        }
+    }
+
+    // for i in 0..3 {
+    //     println!("level {}", i);
+    //     for j in 0..3 {
+    //         for k in 0..3 {
+    //             print!("{:>5}, ", storage_room[i][j][k]);
+    //         }
+    //         println!()
+    //     }
+    //     println!()
+    // }
+
+    return floor_box_count;
+}
+/// Returns an iterator yielding all indexes in parallel.
+fn all_indexes_par(len: usize) -> impl ParallelIterator<Item = (usize, usize, usize)> {
+    (0..len).into_par_iter().flat_map(move |i| (0..len).into_par_iter().flat_map(move |j| (0..len).into_par_iter().map(move |k| (i, j, k))))
+}
+/// Returns an iterator yielding each valid spot's index
+fn valid_indexes_par<'a>(
+    storage_room: &'a Vec<Vec<Vec<bool>>>,
+) -> impl ParallelIterator<Item = (usize, usize, usize)> + 'a {
+    let max_index: usize = storage_room.len() - 1;
+
+    // filter out any indexes that can't support a box
+    return all_indexes_par(max_index).filter(move |&(i, j, k)| {
+        // where are the walls?
+        let is_wall_below = i == 0;
+        let is_wall_left = j == 0;
+        let is_wall_right = j == max_index;
+        let is_wall_behind = k == 0;
+        let is_wall_infront = k == max_index;
 
         // is current index occupied?
         if storage_room[i][j][k] {
@@ -150,96 +233,6 @@ fn valid_indexes<'a>(
 mod test {
     use super::*;
     #[test]
-    fn test_valid_indexes() {
-        let storage_room_1 = vec![
-            vec![
-                // level 0 Floor
-                vec![true, true, false], // front
-                vec![true, false, false],
-                vec![false, false, false], // back
-            ],
-            vec![
-                // level 1
-                vec![false, false, false], // front
-                vec![false, false, false],
-                vec![false, false, false], // back
-            ],
-            vec![
-                // level 2 Ceiling
-                vec![false, false, false], // front
-                vec![false, false, false],
-                vec![false, false, false], // back
-            ],
-        ]; // 3 boxes
-        let storage_room_2 = vec![
-            vec![
-                // level 0 Floor
-                vec![true, true, true], // front
-                vec![true, true, false],
-                vec![true, false, false],
-            ],
-            vec![
-                // level 1
-                vec![true, true, false], // front
-                vec![true, false, false],
-                vec![false, false, false],
-            ],
-            vec![
-                // level 2 Ceiling
-                vec![false, false, false], // front
-                vec![false, false, false],
-                vec![false, false, false],
-            ],
-        ]; // 9 boxes
-        let storage_room_3 = vec![
-            vec![
-                // level 0 Floor
-                vec![true, true, true], // front
-                vec![true, true, true],
-                vec![true, true, true], // back
-            ],
-            vec![
-                // level 1
-                vec![true, true, true], // front
-                vec![true, true, false],
-                vec![true, false, false], // back
-            ],
-            vec![
-                // level 2 Ceiling
-                vec![false, false, false], // front
-                vec![false, false, false],
-                vec![false, false, false], // back
-            ],
-        ]; // 15 boxes
-
-        let expected_valid_storage_room_1 = &[
-            (0, 0, 2),
-            (0, 1, 1),
-            (0, 1, 2),
-            (0, 2, 0),
-            (0, 2, 1),
-            (0, 2, 2),
-            (1, 0, 0),
-        ];
-        let expected_valid_storage_room_2 = &[(0, 1, 2), (0, 2, 1), (0, 2, 2), (2, 0, 0)];
-        let expected_valid_storage_room_3 = &[
-            (1, 1, 2),
-            (1, 2, 1),
-            (1, 2, 2),
-            (2, 0, 0),
-            (2, 0, 1),
-            (2, 1, 0),
-        ];
-
-        let valid_storage_room_1 = valid_indexes(&storage_room_1).collect::<Vec<_>>();
-        let valid_storage_room_2 = valid_indexes(&storage_room_2).collect::<Vec<_>>();
-        let valid_storage_room_3 = valid_indexes(&storage_room_3).collect::<Vec<_>>();
-
-        assert_eq!(valid_storage_room_1, expected_valid_storage_room_1);
-        assert_eq!(valid_storage_room_2, expected_valid_storage_room_2);
-        assert_eq!(valid_storage_room_3, expected_valid_storage_room_3);
-    }
-    #[test]
     fn input_3_output_3() {
         assert_eq!(3, minimum_boxes(3));
     }
@@ -256,8 +249,16 @@ mod test {
         assert_eq!(9, minimum_boxes(15));
     }
     #[test]
+    fn input_126_output_39() {
+        assert_eq!(39, minimum_boxes(126))
+    }
+    #[test]
+    fn input_126_output_39_par() {
+        assert_eq!(39, minimum_boxes_par(126))
+    }
+    #[test]
     fn test_all_indexes() {
-        let all_indexes = all_indexes().collect::<Vec<_>>();
+        let all_indexes = all_indexes(3).collect::<Vec<_>>();
         let expected_all_indexes = &[
             (0, 0, 0),
             (0, 0, 1),
